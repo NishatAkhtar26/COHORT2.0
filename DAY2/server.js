97b479d7b63a6dabd6860b78b431be21ebec7a10
NishatAkhtar26 <nishatakhtaransari@gmail.com>
2026-01-22 21:16:49 +0530

    first commit

---
+++ DAY2/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+
+
+app.get("/",(req,res) => {
+    res.send("Nishat")
+})
+
+
+app.get("/about",function(req,res){
+    res.send("it is an about page")
+})
+app.listen(3000)
