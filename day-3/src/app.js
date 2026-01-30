92528283bcf4d3099c5fe928899d5e7ed1668725
NishatAkhtar26 <nishatakhtaransari@gmail.com>
2026-01-30 20:53:50 +0530

    Day-3

---
+++ day-3/src/app.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json())
+
+
+const notes = [
+    // {
+    //     title: "Title1 1" ,
+    // description: "Description 1"
+    // },
+    // {
+    //     title: "Title 2",
+    //     description: "Description 2"
+    // }
+]
+
+app.post("/notes",(req,res) => {
+    console.log(req.body);
+    notes.push(req.body);
+    console.log(notes);
+    res.send("Note created successfully")
+    // notes.push(req.body);
+    // console.log(req.body);
+})
+
+
+
+
+app.get("/notes", (req,res) => {
+    res.send(notes)
+})
+
+
+app.delete("/notes/:index", (req,res) => {
+    delete notes[req.params.index]
+
+    res.send("notes deleted successfully")
+})
+
+
+app.patch("/notes/:index", (req,res) => {
+    notes [req.params.index].title = req.body.title
+    res.send("notes updated successfully")
+})
+
+
+
+
+module.exports = app;
